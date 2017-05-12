9661f73ff8ed03952a32c4ffdbb54d1140e71d25
Prince of Archives <rich.tijerina@gmail.com>
2017-05-11 20:41:19 -0700

    Update 0_PreBuild.sh

--- 0_PreBuild.sh
+++ 0_PreBuild.sh
@@ -1,1 +1,15 @@
 #!/usr/local/bin/bash
+rel=$1
+fetch https://openbsd.cs.toronto.edu/pub/OpenBSD/6.1/amd64/base$rel.tar.gz;
+fetch https://openbsd.cs.toronto.edu/pub/OpenBSD/6.1/amd64/etc$rel.tar.gz;
+fetch https://openbsd.cs.toronto.edu/pub/OpenBSD/6.1/amd64/comp$rel.tar.gz; 
+fetch https://openbsd.cs.toronto.edu/pub/OpenBSD/6.1/amd64/man$rel.tar.gz;
+fetch https://openbsd.cs.toronto.edu/pub/OpenBSD/6.1/amd64/misc$rel.tar.gz;
+fetch https://openbsd.cs.toronto.edu/pub/OpenBSD/6.1/amd64/xbase$rel.tar.gz;
+fetch https://openbsd.cs.toronto.edu/pub/OpenBSD/6.1/amd64/xfont$rel.tar.gz;
+fetch https://openbsd.cs.toronto.edu/pub/OpenBSD/6.1/amd64/bsd;
+fetch https://openbsd.cs.toronto.edu/pub/OpenBSD/6.1/amd64/xlink$rel.tar.gz;
+fetch https://openbsd.cs.toronto.edu/pub/OpenBSD/6.1/amd64/xserv$rel.tar.gz;
+fetch https://openbsd.cs.toronto.edu/pub/OpenBSD/6.1/amd64/xshare$rel.tar.gz;
+fetch https://openbsd.cs.toronto.edu/pub/OpenBSD/6.1/amd64/bsd.mp;
+fetch https://openbsd.cs.toronto.edu/pub/OpenBSD/6.1/amd64/bsd.rd;
