5ce434f75c91c4def7b4d92427a29cea788cb116
Prince of Archives <rich.tijerina@gmail.com>
2017-10-28 20:05:32 -0700

    Update INSTALL.pl

--- INSTALL.pl
+++ INSTALL.pl
@@ -7,3 +7,17 @@ use HB::PORTINSTALL qw(portinstall);
 use HB::PSKOPIE qw(pskopie);
 use HB::USR qw(usr);
 
+open(my $nfh, '<', '/usb/register`);
+my @lines = <$nfh>;
+close $nfh; chomp @lines;
+my $num = @lines;
+
+print "INSTALL.pl: cmdr name?  ";
+my $cmdr = <STDIN>;
+print "\nINSTALL.pl: cmdr I await your orders!\n";  
+
+print "HUBBUB networks will rise\n";
+print "Behold birth of clone $num\n";
+print "Created after the image of skrp god!\n";
+print "!!! viva skrp iii\n";
+
